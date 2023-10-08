2020c77f6a556fbe0751cbcf576a75e8f7005e23
KONb <ivanomsk7@gmail.com>
2023-10-08 19:18:29 +0300

    test commit

--- src/ru/mirea/task1/opt3/ArraySum.java
+++ src/ru/mirea/task1/opt3/ArraySum.java
@@ -1,5 +1,21 @@
 package ru.mirea.task1.opt3;
 
-public class ArraySum {
+import java.util.Scanner;
 
+public class ArraySum {
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Задание №3\nВведите размер массива: ");
+        int n = sc.nextInt();
+        int[] B = new int[n];
+        int S = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Введите " + (i + 1) + "-ый элемент массива: ");
+            B[i] = sc.nextInt();
+            S += B[i];
+//            System.out.println();
+        }
+        float sr = (float) S / n;
+        System.out.println("Сумма = " + S + "\nСреднее арифметическое = " + sr);
+    }
 }
